1eaf252b51a80bbd5e2aaf71e2e41317c89ed9e7
Silvia Colmenares <Silvianohemy38@gmail.com>
2022-12-10 14:56:38 -0300

    Agregando actualizacion de cookies

--- src/routes/apiLogin.js
+++ src/routes/apiLogin.js
@@ -49,6 +49,7 @@ login.post("/",validarDatos, async (req,res) => {
 
 login.get("/", (req,res) => {
     if (req.session.email) {
+        req.session.touch()//renovar la cookie   poner en un midderware si querres que se actualice en varias rutas distintas
         res.send({
             session: req.session,
             sessionId: req.sessionID,
