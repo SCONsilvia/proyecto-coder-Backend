fa195cdf0bf2dbd7b3297ed4eb95ad8de10ddd20
Silvia Colmenares <Silvianohemy38@gmail.com>
2023-03-26 12:09:59 -0300

    cambiando domain

--- src/services/server.js
+++ src/services/server.js
@@ -27,7 +27,7 @@ const StoreOptions = {
     maxAge: ttlSeconds * 1000,
     cookie: {
         maxAge: ttlSeconds * 1000,
-        domain: "localhost"
+        domain: "railway.app"
     },
 };
 // finsesion
